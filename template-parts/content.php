347cf06ca740e8d0decdcfd5b008f7642b92b50a
KamataRyo <mugil.cephalus+github@gmail.com>
2016-05-27 14:37:31 +0900

    Update content parts

--- template-parts/content.php
+++ template-parts/content.php
@@ -11,37 +11,54 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php
-			if ( is_single() ) {
-				the_title( '<h1 class="entry-title">', '</h1>' );
-			} else {
-				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-			}
 
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
+		<?php if ( 'post' === get_post_type() ) : ?>
+		<div class="entry-meta text-center">
 			<?php birder_posted_on(); ?>
 		</div><!-- .entry-meta -->
 		<?php
 		endif; ?>
+
+		<?php
+			if ( is_single() ) {
+				the_title( '<h1 class="entry-title text-center">', '</h1>' );
+			} else {
+				the_title( '<h2 class="entry-title text-center"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			}
+		?>
+
+		<div class="text-center">
+			<?php
+				if ( has_post_thumbnail() ) {
+					echo get_the_post_thumbnail(get_the_ID() );
+				}
+			?>
+		</div>
+
 	</header><!-- .entry-header -->
 
-	<div class="entry-content">
-		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'birder' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
+	<?php if ( is_single() ): ?>
 
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'birder' ),
-				'after'  => '</div>',
-			) );
-		?>
-	</div><!-- .entry-content -->
+		<div class="entry-content">
+			<?php
+				the_content( sprintf(
+					/* translators: %s: Name of current post. */
+					wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'birder' ), array( 'span' => array( 'class' => array() ) ) ),
+					the_title( '<span class="screen-reader-text">"', '"</span>', false )
+				) );
 
-	<footer class="entry-footer">
-		<?php birder_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'birder' ),
+					'after'  => '</div>',
+				) );
+			?>
+		</div><!-- .entry-content -->
+
+		<footer class="entry-footer">
+			<?php birder_entry_footer(); ?>
+		</footer><!-- .entry-footer -->
+
+	<?php endif; ?>
+
+
 </article><!-- #post-## -->
